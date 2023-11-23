e9427bde7258c7027a9677bf89c373c9b3c2beee
Sven Thiel <m6thsv2@googlemail.com>
2023-11-23 17:26:34 +0100

    #21 remove svelte-kit folder

--- .svelte-kit/generated/client/nodes/13.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/components/form/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/components/form/+page.svelte";
--- .svelte-kit/generated/client/nodes/15.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/components/page/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/components/page/+page.svelte";
